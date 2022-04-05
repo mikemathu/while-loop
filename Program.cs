03540b432ee4b6c1185f0c6d472add39380a07b9
Michael Mathu <mathumichael17@gmail.com>
2022-04-05 17:46:24 +0300

    Added simple number loop

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WhileLooop
+{
+    class Whileloop
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            while(x <= 10)
+            {
+            Console.WriteLine("Number " + x);
+             x++;
+            }
+        }
+    }
+}
