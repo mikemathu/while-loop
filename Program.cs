4508bca334c8e2cbd5edd7e9aefbeb6e8d66178f
Michael Mathu <mathumichael17@gmail.com>
2022-04-05 17:54:18 +0300

    number between 0 and 10 loop

--- Program.cs
+++ Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int x = 1;
+            //SIMPLE WHILE LOOP
+            //int x = 1;
 
-            while(x <= 10)
+            //while(x <= 10)
+            //{
+            //Console.WriteLine("Number " + x);
+            //x++;
+            //}
+
+            //LOOP FOR ENTERING NUMBER BTWN 0 AND 10
+            int playerNumber = -1;
+
+            while (playerNumber < 0 || playerNumber > 10)
             {
-            Console.WriteLine("Number " + x);
-             x++;
+                Console.WriteLine("Enter a number between 0 and 10");
+                string playerResponse = Console.ReadLine();
+                playerNumber = Convert.ToInt32(playerResponse);
             }
         }
     }
